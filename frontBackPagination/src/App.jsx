fd9c1279605e8d2529836bb107e70fb8f12ec33e
Abdul Razzak <abdulrazzak74832@gmail.com>
2024-02-12 14:03:49 +0530

    backend side pagination task project updated

--- frontBackPagination/src/App.jsx
+++ frontBackPagination/src/App.jsx
@@ -6,25 +6,35 @@ import axios from 'axios';
 function App() {
   const [products, setProducts] = useState([]);
   const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
 //   fetch('https://dummyjson.com/products?limit=100')
 // .then(res => res.json())
 // .then(console.log);
-          
+fetch('https://dummyjson.com/products?limit=10&skip=0')
+.then(res => res.json())
+.then(console.log);          
 
 useEffect(() => {
 
   async function fetchProducts(){
-    const data = await axios('https://dummyjson.com/products?limit=100');
-    // const data = await axios(`https://dummyjson.com/products?limit=10?skip=${page * 10 - 10}`);
+    // const data = await axios('https://dummyjson.com/products?limit=100');
+    // setProducts(data.data.products);
+    // console.log('fetched products : ',data);
+    const data = await axios.get(`https://dummyjson.com/products?limit=10&skip=${page * 10 - 10}`);
     setProducts(data.data.products);
-    console.log('fetched products : ',data.data.products);
+    setTotal(data.data.total);
+    console.log('fetched products : ',data.data.total);
   }
   fetchProducts();
-}, [])
+}, [page])
 
   const selectPageHandler = (selectedPage)=>{
     
-    if(selectedPage >= 1 && selectedPage <= products.length / 10 && selectedPage !== page)
+    // if(selectedPage >= 1 && selectedPage <= products.length / 10 && selectedPage !== page)
+    // {
+    //   setPage(selectedPage);
+    // }
+    if(selectedPage >= 1 && selectedPage <= total / 10 && selectedPage !== page)
     {
       setPage(selectedPage);
     }
@@ -37,7 +47,7 @@ useEffect(() => {
         products.length == 0 && <h1>no products have fetched</h1>
       }
       {
-        products.length > 0 && products.slice(page*10 - 10, page*10).map((product,index)=>{
+        products.length > 0 && products.map((product,index)=>{
           return <div className="product" key={index}>
             <img src={product.thumbnail} alt={product.title} />
             <div className="title">{product.title}</div>
@@ -57,11 +67,11 @@ useEffect(() => {
       <div className="pagination">
         <button className={`prev ${page ==1 ? 'prev-disabled': ''}`} onClick={()=>selectPageHandler(page-1)}>prev</button>
         {
-          [...Array(products.length / 10)].map((_,i)=>{
+          [...Array(total / 10)].map((_,i)=>{
             return <div className={`${page-1==i ? 'current':''} page`} onClick={()=>selectPageHandler(i+1)}>{i+1}</div>
           })
         }
-        <button className={`next ${page < products.length / 10 ? '': 'next-disabled'}`} onClick={()=>selectPageHandler(page+1)}>next</button>
+        <button className={`next ${page < total / 10 ? '': 'next-disabled'}`} onClick={()=>selectPageHandler(page+1)}>next</button>
       </div>
      </div>
      }
